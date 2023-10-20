b8f5889cb378ef2fcc0202e7ceae268488687369
Kaifeng Lu <kaifenglu@gmail.com>
2023-10-20 00:15:40 -0400

    rounding for generated time

--- src/dosing_imputation.cpp
+++ src/dosing_imputation.cpp
@@ -3,32 +3,38 @@
 
 using namespace Rcpp;
 
-//' @title Dosing Imputation for Ongoing Patients
+//' @title Dosing Date Imputation for Ongoing Patients
 //' @description Imputes the dosing dates after cutoff for ongoing
 //' patients with dosing records.
 //'
 //' @param usubjid The unique subject ID.
-//' @param V Last dosing visit date relative to randomization.
-//' @param C Cutoff date relative to randomization.
-//' @param D Discontinuation date relative to randomization.
+//' @param V The last dosing visit date relative to randomization.
+//' @param C The cutoff date relative to randomization.
+//' @param D The discontinuation date relative to randomization.
 //' @param model_ki The model for the number of skipped visits between
 //'   two consecutive drug dispensing visits.
 //' @param theta_ki The model parameters for the number of skipped visits
 //'   between two consecutive drug dispensing visits.
-//' @param muT Regression coefficient for the linear model for the gap
-//'   time between two consecutive drug dispensing visits.
-//' @param sigmaT Residual standard deviation for the linear model for
-//'   the gap time between two consecutive drug dispensing visits.
+//' @param muT The regression coefficient for the linear model for the
+//'   gap time between two consecutive drug dispensing visits.
+//' @param sigmaT The residual standard deviation for the linear model
+//'   for the gap time between two consecutive drug dispensing visits.
 //'
-//' @return A data frame with two variables: usubjid and day for the
-//' dosing visit date relative to randomization.
+//' @return A data frame with two variables:
+//'
+//' * \code{usubjid}: The unique subject ID.
+//'
+//' * \code{day}: The dosing visit date relative to randomization.
 //'
 //' @author Kaifeng Lu, \email{kaifenglu@@gmail.com}
 //'
 //' @examples
 //' set.seed(314)
-//' f_dose_ongoing_cpp("A001", 297, 329, 569,
-//'                    "zip", c(0.4, 2.5), 21, 2.3)
+//'
+//' f_dose_ongoing_cpp(
+//'   usubjid = "A001", V = 297, C = 329, D = 569,
+//'   model_ki = "zip", theta_ki = c(0.4, 2.5),
+//'   muT = 21, sigmaT = 2.3)
 //'
 //' @export
 // [[Rcpp::export]]
@@ -76,7 +82,7 @@ DataFrame f_dose_ongoing_cpp(const StringVector usubjid,
 
     // time from Vi to the first drug dispensing visit after data cut
     Ti = rtnormcpp((ki+1)*muT, sigmaT, C(i) - Vi, R_PosInf);
-    Ti = std::max(std::round(Ti), 0.0);
+    Ti = std::max(std::round(Ti), C(i) - Vi + 1.0);
 
     while (Vi + Ti <= D(i)) {
       // next dispensing visit
@@ -109,8 +115,8 @@ DataFrame f_dose_ongoing_cpp(const StringVector usubjid,
         ki = 0;
       }
 
-      // time from Vi to the first drug dispensing visit after data cut
-      Ti = rtnormcpp((ki+1)*muT, sigmaT, C(i) - Vi, R_PosInf);
+      // time from Vi to the next drug dispensing visit
+      Ti = rtnormcpp((ki+1)*muT, sigmaT, 0, R_PosInf);
       Ti = std::max(std::round(Ti), 1.0);
     }
   }
@@ -124,48 +130,56 @@ DataFrame f_dose_ongoing_cpp(const StringVector usubjid,
 
 
 
-//' @title Dosing Imputation for New Patients
-//' @description Imputes the dosing data for new patients and ongoing
+//' @title Dosing Date Imputation for New Patients
+//' @description Imputes the dosing dates for new patients and ongoing
 //' patients with no dosing records.
 //'
 //' @param usubjid The unique subject ID.
-//' @param V initialized to 0 corresponding to the randomization visit.
-//' @param C Cutoff date relative to randomization.
-//' @param D Discontinuation date relative to randomization.
+//' @param V Initialized to 0 and corresponds to the randomization visit.
+//' @param C The cutoff date relative to randomization.
+//' @param D The discontinuation date relative to randomization.
 //' @param model_k0 The model for the number of skipped visits between
 //'   randomization and the first drug dispensing visit.
 //' @param theta_k0 The model parameters for the number of skipped
 //'   visits between randomization and the first drug dispensing visit.
 //' @param model_t0 The model for the gap time between randomization
-//'   and the first drug dispensing visit with no skipping.
+//'   and the first drug dispensing visit when there is no visit skipping.
 //' @param theta_t0 The model parameters for the gap time between
-//'   randomization and the first drug dispensing visit with no skipping.
-//' @param mu0 Regression coefficient for the linear model for the gap
-//'   time between randomization and the first drug dispensing visit.
-//' @param sigma0 Standard deviation for the linear model for the gap
-//'   time between randomization and the first drug dispensing visit.
+//'   randomization and the first drug dispensing visit when there is
+//'   no visit skipping.
+//' @param mu0 The regression coefficient for the linear model for the gap
+//'   time between randomization and the first drug dispensing visit
+//'   when there is visit skipping.
+//' @param sigma0 The residual standard deviation for the linear model
+//'   for the gap time between randomization and the first drug
+//'   dispensing visit when there is visit skipping.
 //' @param model_ki The model for the number of skipped visits between
 //'   two consecutive drug dispensing visits.
 //' @param theta_ki The model parameters for the number of skipped
 //'   visits between two consecutive drug dispensing visits.
-//' @param muT Regression coefficient for the linear model for the gap
+//' @param muT The regression coefficient for the linear model for the gap
 //'   time between two consecutive drug dispensing visits.
-//' @param sigmaT Residual standard deviation the linear model for the
+//' @param sigmaT The residual standard deviation the linear model for the
 //'   gap time between two consecutive drug dispensing visits.
 //'
-//' @return A data frame with two variables: usubjid and day for the
-//'   dosing visit date relative to randomization.
+//' @return A data frame with two variables:
+//'
+//' * \code{usubjid}: The unique subject ID.
+//'
+//' * \code{day}: The dosing visit date relative to randomization.
 //'
 //' @author Kaifeng Lu, \email{kaifenglu@@gmail.com}
 //'
 //' @examples
 //' set.seed(529)
-//' f_dose_new_cpp("Z001", 0, 87, 985,
-//'                "zip", c(0.6, 1.1),
-//'                "log-logistic", c(-1.0, 0.7),
-//'                21.5, 1.9,
-//'                "zip", c(0.1, 0.4),
-//'                21, 2.3)
+//'
+//' f_dose_new_cpp(
+//'   usubjid = "Z001", V = 0, C = 87, D = 985,
+//'   model_k0 = "zip", theta_k0 = c(0.6, 1.1),
+//'   model_t0 = "log-logistic", theta_t0 = c(-1.0, 0.7),
+//'   mu0 = 21.5, sigma0 = 1.9,
+//'   model_ki = "zip", theta_ki = c(0.1, 0.4),
+//'   muT = 21, sigmaT = 2.3)
 //'
 //' @export
 // [[Rcpp::export]]
@@ -222,7 +236,10 @@ DataFrame f_dose_new_cpp(const StringVector usubjid,
     // generate the gap time between randomization and the first
     // drug dispensing visit
     if (ki == 0) {
-      if (C(i) >= 0) { // draw from truncated distributions
+      // simulate the gap time between randomization and the first
+      // drug dispensing visit in the absence of skipping visits
+      if (C(i) >= 0) {
+        // draw from truncated distributions for ongoing patients
         if (model_t0 == "constant") {
           if (theta_t0(0) >= C(i) - Vi) {
             Ti = theta_t0(0);
@@ -245,8 +262,11 @@ DataFrame f_dose_new_cpp(const StringVector usubjid,
                              R_PosInf));
         } else {
           Ti = C(i) - Vi;
+          stop("incorrect model for T0");
         }
+        Ti = std::max(std::floor(Ti), C(i) - Vi + 1.0);
       } else {
+        // draw from regular distributions for new patients
         if (model_t0 == "constant") {
           Ti = theta_t0(0);
         } else if (model_t0 == "exponential") {
@@ -260,15 +280,19 @@ DataFrame f_dose_new_cpp(const StringVector usubjid,
         } else {
           Ti = 0;
         }
+        Ti = std::max(std::floor(Ti), 0.0);
       }
     } else {
-      if (C(i) >= 0) { // draw from truncated normal
+      // simulate the gap time between randomization and the first
+      // drug dispensing visit in the presence of skipping visits
+      if (C(i) >= 0) { // draw from truncated normal for ongoing patients
         Ti = rtnormcpp(ki*mu0, sigma0, C(i) - Vi, R_PosInf);
-      } else {
+        Ti = std::max(std::round(Ti), C(i) - Vi + 1.0);
+      } else { // draw from regular normal for new patients
         Ti = R::rnorm(ki*mu0, sigma0);
+        Ti = std::max(std::round(Ti), 1.0);
       }
     }
-    Ti = std::max(std::round(Ti), 0.0);
 
     // generate drug dispensing visits
     while (Vi + Ti <= D(i)) {
@@ -302,8 +326,8 @@ DataFrame f_dose_new_cpp(const StringVector usubjid,
         ki = 0;
       }
 
-      // time from Vi to the first drug dispensing visit after data cut
-      Ti = rtnormcpp((ki+1)*muT, sigmaT, C(i) - Vi, R_PosInf);
+      // time from Vi to the next drug dispensing visit
+      Ti = rtnormcpp((ki+1)*muT, sigmaT, 0, R_PosInf);
       Ti = std::max(std::round(Ti), 1.0);
     }
   }
